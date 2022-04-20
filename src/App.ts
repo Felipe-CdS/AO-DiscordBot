be89ee6d9d1c028609d1518457e1164dc7bcdff4
Felipe-CdS <felipe32santos@hotmail.com>
2022-04-20 11:46:25 -0300

    heroku adjustments

--- src/App.ts
+++ src/App.ts
@@ -9,4 +9,4 @@ app.use(express.json({ verify: VerifyDiscordRequest }));
 
 app.use(router);
 
-app.listen(3000, () => { console.log('Listening on port 3000') });
+app.listen(process.env.PORT || 3000, () => { console.log("Server Up!") });
--- src/utils.ts
+++ src/utils.ts
@@ -4,17 +4,15 @@ import { Request, Response } from 'express';
 export function VerifyDiscordRequest(){
 	return(
 		function(req: Request, res: Response, buf: any, encoding: any){
-
-		const publicKey = process.env.PUBLIC_KEY				|| "";
-		const signature = req.header('x-signature-ed25519')		|| "";
-		const timestamp = req.header('x-signature-timestamp')	|| "";
-	  
-		const isVerified = verifyKey(buf, signature, timestamp, publicKey);
-		
-		if (!isVerified) {
-		  res.status(401);
-		  return;
+			const publicKey = process.env.PUBLIC_KEY as string;
+			const signature = req.header('x-signature-ed25519') as string;
+			const timestamp = req.header('x-signature-timestamp') as string;		  
+			const isVerified = verifyKey(buf, signature, timestamp, publicKey);
+			
+			if (!isVerified) {
+			  res.status(401);
+			  return;
+			}
 		}
-	  }
 	);
 }
